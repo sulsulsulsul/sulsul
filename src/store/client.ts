a874d99fe904e7935f902d295ac0c1851e07c35d
eugene9851 <eugene9851@naver.com>
2024-07-28 22:16:24 +0900

    Feat: 유저 정보 상태 저장 및 관리

--- src/store/client.ts
+++ src/store/client.ts
@@ -1,1 +1,54 @@
 import { create } from 'zustand'
+
+interface Job {
+  jobId: number
+  name: string
+}
+
+interface AuthState {
+  userId: number
+  token: string
+}
+
+interface DataState {
+  email: string
+  job: Job
+  nickname: string
+  userId: number
+}
+
+interface UserState {
+  auth: AuthState
+  data: DataState
+  image: string
+  setUserInfo: (userInfo: Partial<UserState>) => void
+}
+
+export const useUserStore = create<UserState>((set) => ({
+  auth: {
+    userId: 0,
+    token: '',
+  },
+  data: {
+    email: '',
+    job: {
+      jobId: 0,
+      name: '',
+    },
+    nickname: '',
+    userId: 0,
+  },
+  image: '',
+  setUserInfo: (userInfo) =>
+    set((state) => ({
+      auth: {
+        ...state.auth,
+        ...userInfo.auth,
+      },
+      data: {
+        ...state.data,
+        ...userInfo.data,
+      },
+      image: userInfo.image ?? state.image,
+    })),
+}))
