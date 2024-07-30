11bee032afc55d93cad3e908d5a71a80683df628
eugene9851 <eugene9851@naver.com>
2024-07-30 14:50:24 +0900

    Feat: 피드백 받기 버튼 클릭 시 피드백 생성 시작

--- src/entities/archives/components/feedback-section-idle/index.tsx
+++ src/entities/archives/components/feedback-section-idle/index.tsx
@@ -1,23 +1,30 @@
 import { HTMLAttributes } from 'react'
 
 import { Button } from '@/components/ui/button'
+import { useCreateFeedback } from '@/entities/feedbacks/hooks/use-create-feedback'
 import { cn } from '@/lib/utils'
 interface FeedbackSectionIdleProps extends HTMLAttributes<HTMLDivElement> {
-  handleCreateFeedback: () => void
+  questionId: number
 }
 
 export const FeedbackSectionIdle = ({
   className,
-  handleCreateFeedback,
+  questionId,
   ...props
 }: FeedbackSectionIdleProps) => {
+  const { mutate: createFeedbackMutation } = useCreateFeedback()
+
+  const handleCreateFeedback = () => {
+    createFeedbackMutation({ questionId })
+  }
+
   return (
     <div className={cn(className)} {...props}>
       <div className="flex h-[157px] flex-col items-center justify-center rounded-base bg-gray-50 text-sm text-gray-500">
         <p>피드백 받기 버튼을 누르면</p>
         <p>내 답변에 대한 피드백을 받을 수 있어요!</p>
         <div className="mt-4">
-          <Button size={'sm'} onClick={handleCreateFeedback}>
+          <Button size="sm" onClick={handleCreateFeedback}>
             <span className="text-base font-semibold">피드백 받기</span>
           </Button>
         </div>
