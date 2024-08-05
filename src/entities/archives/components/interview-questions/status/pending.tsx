65538ea108b156964abc4783c925bfb6d7349520
eugene9851 <eugene9851@naver.com>
2024-08-05 16:47:18 +0900

    Feat: 문제 예측 애니메이션

--- src/entities/archives/components/interview-questions/status/pending.tsx
+++ src/entities/archives/components/interview-questions/status/pending.tsx
@@ -10,19 +10,18 @@ export const PendingStatus = ({
 }: PendingInterviewQuestionProps) => {
   return (
     <div className={cn('h-full overflow-hidden', className)} {...props}>
-      <div className="flex size-full flex-col gap-2">
+      <div className="flex size-full flex-col gap-2 ">
         {Array(9)
           .fill(0)
           .map((_, i) => (
             <div
               key={i}
-              style={{
-                animationDelay: `${(Math.random() * i) / 10}s`,
-              }}
               className={cn(
-                'h-[60vh] w-full animate-loading-expand-width rounded-md bg-gray-200',
+                'relative h-[65vh] w-full rounded-md bg-gray-100 overflow-hidden card--skeleton',
               )}
-            />
+            >
+              <div className="absolute inset-0 animate-shimmer" />
+            </div>
           ))}
       </div>
     </div>
