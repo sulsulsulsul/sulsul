8e7b3a12ffaf14f6c404242105532c8e55dd0bda
eugene9851 <eugene9851@naver.com>
2024-08-06 00:21:25 +0900

    Feat: useCurrentArchiveIdStore, useCreateQuestionStore 상태 관리

---
+++ src/components/shared/modal.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog'
+
+import alert from '/public/images/icons/alert.svg'
+
+interface AlertModalProps {
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+  title: string
+  desc: string
+  action: string
+}
+
+export const AlertModal = ({
+  onClick,
+  title,
+  desc,
+  action,
+}: AlertModalProps) => {
+  return (
+    <AlertDialogContent>
+      <AlertDialogHeader className="items-center">
+        <Image src={alert} alt="경고아이콘" />
+        <AlertDialogTitle>{title}</AlertDialogTitle>
+        <AlertDialogDescription>{desc}</AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter className="justify-center">
+        <AlertDialogCancel className="grow">취소하기</AlertDialogCancel>
+        <AlertDialogAction
+          onClick={onClick}
+          className="grow bg-blue-500 text-white"
+        >
+          {action}
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  )
+}
