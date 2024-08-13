e8b9991522c89c8eaab7279816b6792104939710
eugene9851 <eugene9851@naver.com>
2024-08-13 12:59:16 +0900

    Feat: 스켈레톤 애니메이션 UI 수정

--- src/entities/archives/components/interview-questions/status/pending.tsx
+++ src/entities/archives/components/interview-questions/status/pending.tsx
@@ -1,29 +1,17 @@
 import { HTMLAttributes } from 'react';
 
-import { cn } from '@/lib/utils';
+import { Skeleton } from '@/components/ui/skeleton';
+
 interface PendingInterviewQuestionProps
   extends HTMLAttributes<HTMLDivElement> {}
-
-export const PendingStatus = ({
-  className,
-  ...props
-}: PendingInterviewQuestionProps) => {
+export function PendingStatus({ className }: PendingInterviewQuestionProps) {
   return (
-    <div className={cn('h-full overflow-hidden', className)} {...props}>
-      <div className="flex size-full flex-col gap-2 ">
-        {Array(9)
-          .fill(0)
-          .map((_, i) => (
-            <div
-              key={i}
-              className={cn(
-                'relative h-[65vh] w-full rounded-md bg-gray-100 overflow-hidden card--skeleton',
-              )}
-            >
-              <div className="absolute inset-0 animate-shimmer" />
-            </div>
-          ))}
-      </div>
+    <div className="flex flex-col">
+      {Array(9)
+        .fill(0)
+        .map((_, i) => (
+          <Skeleton key={i} className="mb-2 h-[65px] w-full bg-gray-100" />
+        ))}
     </div>
   );
-};
+}
