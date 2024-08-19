e869cae70aa412beadde84c4408f6a6f43660b68
robin <nowrobin3@gmail.com>
2024-08-20 00:03:00 +0900

    refact observer & setinterval

--- src/app/(landing)/components/challenge/index.tsx
+++ src/app/(landing)/components/challenge/index.tsx
@@ -8,44 +8,40 @@ export default function Challenge() {
   const [currentImage, setCurrentImage] = useState(0);
   const MaxLoop = 4;
   const ref = useRef(null);
-  const myElementRef = useRef(null); // 감시할 요소를 참조하기 위한 useRef
-  const [isInView, setIsInView] = useState(false); // 요소가 화면에 보이는지 상태로 관리
+  const [isInView, setIsInView] = useState(false);
 
   useEffect(() => {
-    // IntersectionObserver의 콜백 함수
     const observerCallback = (entries: IntersectionObserverEntry[]) => {
       entries.forEach((entry) => {
-        setIsInView(entry.isIntersecting); // 요소가 화면에 보이면 상태를 true로 업데이트
+        setIsInView(entry.isIntersecting);
       });
     };
-    // IntersectionObserver 인스턴스를 생성
     const observer = new IntersectionObserver(observerCallback, {
-      root: null, // 뷰포트를 기준으로 함
+      root: null,
       rootMargin: '0px',
-      threshold: 0.1, // 요소의 10%가 화면에 보일 때 콜백을 실행
+      threshold: 0.1,
     });
     if (ref.current) {
-      observer.observe(ref.current); // 요소를 관찰 시작
+      observer.observe(ref.current);
     }
-    // 클린업 함수: 컴포넌트가 언마운트될 때 observer를 해제
     return () => {
       if (ref.current) {
         observer.unobserve(ref.current);
       }
     };
-  }, []); // 빈 배열을 사용해 컴포넌트가 처음 마운트될 때만 실행
+  }, []);
 
   useEffect(() => {
-    const intervalRef = setInterval(() => {
-      setCurrentImage((prev) => {
-        return prev >= MaxLoop ? 0 : (prev += 1);
-      });
-      console.log(isInView);
-    }, 500);
-    if (!isInView) {
+    if (isInView) {
+      const intervalRef = setInterval(() => {
+        setCurrentImage((prev) => {
+          return prev >= MaxLoop ? 0 : (prev += 1);
+        });
+      }, 1200);
       return () => clearInterval(intervalRef);
     }
-  }, []);
+  }, [isInView]);
+  console.log(isInView);
 
   return (
     <div
