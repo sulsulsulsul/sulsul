236cfd986dc2d1599863c400cda4bd0b95c62077
dudgns0612 <dudgns0612@naver.com>
2024-09-09 22:19:45 +0900

    feat: 대시보드 내연습현황 기간 변경 작업

--- src/lib/backend-api/api-end-point.ts
+++ src/lib/backend-api/api-end-point.ts
@@ -147,7 +147,7 @@ export const API_ENDPOINT = {
       return {
         url: `${API_V1_BASE}/questions/search`,
         method: Method.GET,
-        authorization: true,
+        authorization: false,
       };
     },
     getSearchQuestions: (query: string) => {
@@ -205,14 +205,14 @@ export const API_ENDPOINT = {
       return {
         url: `${API_V1_BASE}/practice/statistics/summary`,
         method: Method.GET,
-        authorization: true,
+        authorization: false,
       };
     },
     getStatisticsDetail: (period: Period) => {
       return {
         url: `${API_V1_BASE}/practice/statistics/detail/${period}`,
         method: Method.GET,
-        authorization: true,
+        authorization: false,
       };
     },
     createPractice: () => {
