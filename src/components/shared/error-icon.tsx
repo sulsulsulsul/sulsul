5a5cfeb2b497b288b9d6723762e822afd59507c9
eugene9851 <eugene9851@naver.com>
2024-09-09 20:32:07 +0900

    fix: import 순서 변경

--- src/components/shared/error-icon.tsx
+++ src/components/shared/error-icon.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+
 import error from '/public/images/icons/toast-error.svg';
 
 export const ErrorIcon = () => {
--- src/components/shared/success-icon.tsx
+++ src/components/shared/success-icon.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+
 import success from '/public/images/icons/toast-success.svg';
 
 export const SuccessIcon = () => {
