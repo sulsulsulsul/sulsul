f04c8c49b31257ba42ce39c9a78dec17984ac301
eugene9851 <eugene9851@naver.com>
2024-09-09 20:39:40 +0900

    fix: import 순서 변경

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,10 +5,10 @@ import { SessionProvider } from 'next-auth/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { ErrorIcon } from '@/components/shared/error-icon';
+import { SuccessIcon } from '@/components/shared/success-icon';
 import { Toaster } from '@/components/ui/sonner';
 import { getQueryClient } from '@/lib/tanstack-query/client';
-import { SuccessIcon } from '@/components/shared/success-icon';
-import { ErrorIcon } from '@/components/shared/error-icon';
 
 interface ProvidersProps extends PropsWithChildren {
   session?: Session;
