479f40960d38f1bb064f1b120f19eb996991fa9e
hyunjun9788 <namhyunjun9788@gmail.com>
2024-09-12 23:57:42 +0900

    feat: 내활동 섹션 UI 구현

---
+++ src/entities/solved/components/my-activity-section/index.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+export const MyActivitySection = () => {
+  return (
+    <div className="mt-[6px] flex w-[282px] flex-col gap-2">
+      <div className="flex items-center gap-1">
+        <Image
+          src="/images/icons/icon-my-activity.svg"
+          width={24}
+          height={24}
+          alt="icon"
+        />
+        <h3 className="text-lg font-bold">내 활동</h3>
+      </div>
+      <div className="flex h-[130px] w-full flex-col items-center justify-center rounded-md border border-gray-200 bg-white px-5 shadow-base">
+        <div className="flex w-full flex-col gap-1 text-lg">
+          <h3 className="font-bold">내가 받은 추천수</h3>
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between font-semibold">
+              <div className="text-gray-500">이번달</div>
+              <div className="text-blue-500">43</div>
+            </div>
+            <div className="flex justify-between font-semibold">
+              <div className="text-gray-500">전체</div>
+              <div className="text-blue-500">128</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/views/solved/index.tsx
+++ src/views/solved/index.tsx
@@ -1,8 +1,14 @@
+import { MyActivitySection } from '@/entities/solved/components/my-activity-section';
 import { TogetherSolvedSection } from '@/entities/solved/components/together-solved-section';
+import { WeekRankingSection } from '@/entities/solved/components/week-ranking-section';
 
 export const Solved = () => {
   return (
-    <main className="px-5">
+    <main className="flex w-full gap-6 px-5">
+      <div className="flex flex-col gap-[30px]">
+        <MyActivitySection />
+        <WeekRankingSection />
+      </div>
       <TogetherSolvedSection />
     </main>
   );
