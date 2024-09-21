2ad2f21c17b1f5693c097de57d901567e6c75a2b
hyunjun9788 <namhyunjun9788@gmail.com>
2024-09-22 01:13:14 +0900

    feat: 답변 모달 버튼 섹션 UI 및 기능 구현

---
+++ src/entities/solved/write-answer-modal/components/button-section.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@/components/ui/button';
+interface ButtonSectionProps {
+  charCount: number;
+  setModal: (state: boolean) => void;
+}
+export const ButtonSection = ({ charCount, setModal }: ButtonSectionProps) => {
+  return (
+    <div className="flex justify-between">
+      <p className="text-sm">
+        💌 BEST 답변으로 3회 이상 선정 시, 커피 쿠폰을 보내드려요!
+      </p>
+      <div className="flex gap-2">
+        <Button
+          size="sm"
+          variant="default"
+          className="h-[42px] w-[66px] border border-gray-300 bg-white text-gray-600"
+          onClick={() => setModal(false)}
+        >
+          취소
+        </Button>
+        <Button
+          size="sm"
+          className="h-[42px] w-[101px]"
+          variant="default"
+          disabled={charCount === 0}
+        >
+          답변 남기기
+        </Button>
+      </div>
+    </div>
+  );
+};
---
+++ src/entities/solved/write-answer-modal/index.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { Dispatch, HTMLAttributes, SetStateAction, useState } from 'react';
+
+import { cn } from '@/lib/utils';
+
+import { ButtonSection } from './components/button-section';
+import { ModalHeader } from './components/hedaer-section';
+import { TextAreaSection } from './components/text-area-section';
+
+interface WriteSolvedModalProp extends HTMLAttributes<HTMLDivElement> {
+  setModal: Dispatch<SetStateAction<boolean>>;
+}
+
+export const WriteSolvedModal = ({
+  setModal,
+  className,
+}: WriteSolvedModalProp) => {
+  const [charCount, setCharCount] = useState(0);
+  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCharCount(e.target.value.length);
+  };
+  return (
+    <div
+      className={cn(
+        'fixed flex  w-screen h-screen top-0 left-0 z-[60] bg-gray-800/80 items-center justify-center',
+      )}
+    >
+      <div className="flex min-h-[426px] w-[624px] flex-col gap-4 rounded-md border border-gray-100 bg-white p-[30px]">
+        <div className="flex flex-col gap-6">
+          <ModalHeader charCount={charCount} />
+          <TextAreaSection handleInput={handleInput} charCount={charCount} />
+        </div>
+        <ButtonSection charCount={charCount} setModal={setModal} />
+      </div>
+    </div>
+  );
+};
