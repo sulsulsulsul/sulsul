1256f1f9a383f0bbd50915044c759f47b314e847
yeooonn <rladuswjd0303@naver.com>
2024-09-28 03:19:38 +0900

    Feat: 최다 빈출 기본질문 리스트 api 연동 완료

--- src/entities/types/challenges.ts
+++ src/entities/types/challenges.ts
@@ -2,3 +2,24 @@ export interface MyChallengesProgressData {
   category: string;
   count: number;
 }
+
+export interface MyQuestionList {
+  answerRate: number;
+  challenges: [
+    {
+      content: string;
+      isAnswered: false;
+      question: {
+        questionId: number;
+        answer: null | string;
+        keywords: [
+          {
+            keywordId: number;
+            content: string;
+          },
+        ];
+        feedback: null | string;
+      };
+    },
+  ];
+}
