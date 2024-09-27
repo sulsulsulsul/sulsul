8a44d1a83021cff6da1cbe7b475c6f52a7b8b4b9
yeooonn <rladuswjd0303@naver.com>
2024-09-28 03:17:05 +0900

    Feat: 최다 빈출 기본질문 리스트 상세 아코디언 UI 구현 중

--- src/app/(routes)/solved/question/components/response-completion-rate/character/index.tsx
+++ src/app/(routes)/solved/question/components/response-completion-rate/character/index.tsx
@@ -5,8 +5,8 @@ const Character = () => {
     <div className="flex flex-col items-center gap-2">
       <Image
         src="/images/solved/character_text.svg"
-        width={160}
-        height={116}
+        width={153}
+        height={74}
         alt="character text"
       />
       <Image
