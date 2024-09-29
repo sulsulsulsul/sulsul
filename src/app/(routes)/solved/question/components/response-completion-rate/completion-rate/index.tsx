0025f0758e7c8fbd2f1e61ccbd2ee6d21fa8120d
yeooonn <rladuswjd0303@naver.com>
2024-09-30 02:15:35 +0900

    Fix: build 실패 수정

--- src/app/(routes)/solved/question/components/response-completion-rate/completion-rate/index.tsx
+++ src/app/(routes)/solved/question/components/response-completion-rate/completion-rate/index.tsx
@@ -1,23 +1,16 @@
-import { useEffect, useState } from 'react';
+'use client';
 import { LineProgressBar } from '@frogress/line';
 
 const CompletionRate = () => {
-  const [rate, setRate] = useState<number | null>(null);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const storedRate = sessionStorage.getItem('Response Completion Rate');
-      setRate(storedRate ? Number(storedRate) : null);
-    }
-  }, []);
+  const storedRate = sessionStorage.getItem('Response Completion Rate');
 
   return (
     <div className="mb-[32px] mt-[20px]">
       <div className="mb-[10px] flex justify-between">
         <p className="text-base font-bold text-gray-800">답변 작성률 </p>
-        <p className="text-base font-semibold text-gray-600">{rate}%</p>
+        <p className="text-base font-semibold text-gray-600">{storedRate}%</p>
       </div>
-      <LineProgressBar percent={Number(rate)} rounded={36} height={8} />
+      <LineProgressBar percent={Number(storedRate)} rounded={36} height={8} />
     </div>
   );
 };
