e4db70900543c3fb5551ee53b18495ac41f4daa3
yeooonn <rladuswjd0303@naver.com>
2024-09-30 02:09:11 +0900

    Fix: 진행 상태 별 버튼 ux 수정

--- src/app/(routes)/solved/question/components/response-completion-rate/vertical-linear-stepper/index.tsx
+++ src/app/(routes)/solved/question/components/response-completion-rate/vertical-linear-stepper/index.tsx
@@ -9,13 +9,14 @@ import { useUserChallengesProgress } from '../../../hook/use-user-challenges-pro
 // 각 카테고리에 대한 설명 및 타입 매핑
 type CategoryInfo = {
   description: string;
+  totalCount: number[];
 };
 
 const categoryInfo: Record<string, CategoryInfo> = {
-  BASIC: { description: '최다 빈출 기본질문' },
-  JOB: { description: '직무역량 & 경험' },
-  CULTURE: { description: '회사 로열티 & 컬쳐핏' },
-  VISION: { description: '가치관 & 비전' },
+  BASIC: { description: '최다 빈출 기본질문', totalCount: [10] },
+  JOB: { description: '직무역량 & 경험', totalCount: [18, 18] },
+  CULTURE: { description: '회사 로열티 & 컬쳐핏', totalCount: [19, 20] },
+  VISION: { description: '가치관 & 비전', totalCount: [15] },
 };
 
 // category에 같은 글자가 포함된 항목들을 묶고 count를 배열로 받는 함수
@@ -26,6 +27,7 @@ const aggregateSteps = (steps: any) => {
       category: string;
       description: string;
       counts: number[];
+      totalCount: number[];
     }
   > = {};
 
@@ -38,6 +40,7 @@ const aggregateSteps = (steps: any) => {
         category: key,
         description: categoryInfo[key].description,
         counts: [],
+        totalCount: categoryInfo[key].totalCount,
       };
     }
 
@@ -66,12 +69,12 @@ const VerticalLinearStepper = ({ accessToken }: VerticalLinearStepperProps) => {
   });
 
   const testDummyData = [
-    { category: 'BASIC', count: 10 },
-    { category: 'JOB_1', count: 10 },
-    { category: 'JOB_2', count: 0 },
-    { category: 'CULTURE_1', count: 0 },
-    { category: 'CULTURE_2', count: 0 },
-    { category: 'VISION', count: 0 },
+    { category: 'BASIC', count: 10, totalCount: 10 },
+    { category: 'JOB_1', count: 8, totalCount: 18 },
+    { category: 'JOB_2', count: 0, totalCount: 18 },
+    { category: 'CULTURE_1', count: 0, totalCount: 19 },
+    { category: 'CULTURE_2', count: 0, totalCount: 20 },
+    { category: 'VISION', count: 0, totalCount: 15 },
   ];
 
   const steps = aggregateSteps(data);
@@ -85,7 +88,8 @@ const VerticalLinearStepper = ({ accessToken }: VerticalLinearStepperProps) => {
             <div>
               <span
                 className={`inline-block size-[12px] rounded-full ${
-                  step.counts.some((count) => count > 0)
+                  step.counts.some((count) => count > 0) ||
+                  step.description === '최다 빈출 기본질문'
                     ? 'bg-blue-500'
                     : 'bg-gray-300'
                 }`}
@@ -112,7 +116,7 @@ const VerticalLinearStepper = ({ accessToken }: VerticalLinearStepperProps) => {
                     step.counts.length > 1 &&
                     idx === 0
                   ) {
-                    if (prevStep.counts[0] === 10) {
+                    if (prevStep.counts[0] === prevStep.totalCount[0]) {
                       showSubmitButton = true;
                     }
                   }
@@ -125,7 +129,7 @@ const VerticalLinearStepper = ({ accessToken }: VerticalLinearStepperProps) => {
                     step.counts.length > 1 &&
                     idx === 0
                   ) {
-                    if (prevStep.counts[1] === 10) {
+                    if (prevStep.counts[1] === prevStep.totalCount[1]) {
                       showSubmitButton = true;
                     }
                   }
@@ -134,7 +138,7 @@ const VerticalLinearStepper = ({ accessToken }: VerticalLinearStepperProps) => {
                   // 1-3-1. 이전 스텝의 마지막 진행 단계가 "작성완료" 일 경우
                   // 1-3-2. 현재 스텝의 진행 단계 버튼을 "작성하기" 버튼으로 수정
                   if (prevStep.counts.length > 1 && step.counts.length === 1) {
-                    if (prevStep.counts[1] === 10) {
+                    if (prevStep.counts[1] === prevStep.totalCount[1]) {
                       showSubmitButton = true;
                     }
                   }
@@ -144,7 +148,7 @@ const VerticalLinearStepper = ({ accessToken }: VerticalLinearStepperProps) => {
                 // 2-1. 첫번째 진행 단계 버튼이 "작성완료"일 경우
                 // 2-2. 현재 스텝의 두번째 진행 단계 버튼을 "작성하기" 버튼으로 수정
                 if (step.counts.length > 1 && idx === 1) {
-                  if (step.counts[0] === 10) {
+                  if (step.counts[0] === step.totalCount[0]) {
                     showSubmitButton = true;
                   }
                 }
@@ -161,25 +165,19 @@ const VerticalLinearStepper = ({ accessToken }: VerticalLinearStepperProps) => {
                     key={idx}
                   >
                     <p className="text-sx ml-[15px] whitespace-normal font-medium text-gray-500">
-                      {count} /{' '}
-                      {step.description === '최다 빈출 기본질문'
-                        ? '10개'
-                        : step.description === '직무역량 & 경험'
-                          ? '18개'
-                          : step.description === '회사 로열티 & 컬쳐핏' &&
-                              idx === 0
-                            ? '19개'
-                            : step.description === '회사 로열티 & 컬쳐핏' &&
-                                idx === 1
-                              ? '20개'
-                              : '15개'}
+                      {count} / {step.totalCount[idx]}
                     </p>
 
-                    {count === 10 ? (
+                    {step.description === '최다 빈출 기본질문' &&
+                    count !== step.totalCount[idx] ? (
+                      <button className="float-right h-[32px] w-[68px] rounded-sm bg-blue-100 text-xs text-blue-500">
+                        작성 중
+                      </button>
+                    ) : count === step.totalCount[idx] ? (
                       <button className="float-right h-[32px] w-[68px] rounded-sm bg-green-100 text-xs text-green-900 shadow-[0_0_0_7px_rgba(255,255,255,1)] transition-all duration-300 hover:h-[32px] hover:w-[68px] hover:shadow-[0_0_0_7px_rgba(163,226,206,1)]">
                         작성완료
                       </button>
-                    ) : count > 0 && count !== 10 ? (
+                    ) : count > 0 && count !== step.totalCount[idx] ? (
                       <button className="float-right h-[32px] w-[68px] rounded-sm bg-blue-100 text-xs text-blue-500">
                         작성 중
                       </button>
@@ -188,7 +186,7 @@ const VerticalLinearStepper = ({ accessToken }: VerticalLinearStepperProps) => {
                         작성하기
                       </button>
                     ) : (
-                      <button className="float-right h-[32px] w-[68px] rounded-sm bg-gray-100 text-xs text-gray-500">
+                      <button className="float-right h-[32px] w-[68px] cursor-default rounded-sm bg-gray-100 text-xs text-gray-500">
                         작성 전
                       </button>
                     )}
