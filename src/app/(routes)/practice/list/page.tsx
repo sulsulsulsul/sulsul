4fa095ea9d957276922a37d1399054f10c8f47a4
robin <nowrobin3@gmail.com>
2024-10-02 12:48:52 +0900

    fix: prefetchQuery error resolve

--- src/app/(routes)/practice/list/page.tsx
+++ src/app/(routes)/practice/list/page.tsx
@@ -7,13 +7,15 @@ import {
 } from '@tanstack/react-query';
 
 import { getPrefecthList } from '@/entities/practice/practice-list/actions/get-prefetch-action';
+import { useUserStore } from '@/store/client';
 import { PracticeHistory } from '@/views/practice-history';
 
 const Page = async () => {
   const queryClient = new QueryClient();
+  const { auth } = useUserStore();
   await queryClient.prefetchQuery({
-    queryKey: ['practiceCount'],
-    queryFn: () => getPrefecthList(),
+    queryKey: ['practiceCount', auth.userId],
+    queryFn: () => getPrefecthList(auth.userId),
   });
   const dehydratedState = dehydrate(queryClient);
 
