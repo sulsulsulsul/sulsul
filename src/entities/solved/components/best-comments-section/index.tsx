ed8b96da9b1a0f89808dc0f8d9c8d588ed233777
hyunjun9788 <namhyunjun9788@gmail.com>
2024-10-04 17:26:02 +0900

    fix: 지난주 BEST 답변 섹션 레이아웃 수정

--- src/entities/solved/components/best-comments-section/index.tsx
+++ src/entities/solved/components/best-comments-section/index.tsx
@@ -59,48 +59,53 @@ export const BestCommentsSection = ({
           />
           <h3 className="text-lg font-bold">지난주 BEST 답변</h3>
         </div>
-        <div className="flex h-[478px] w-full flex-col items-center justify-center gap-4 rounded-md border border-gray-200 bg-white px-5 shadow-base">
+        <div className="relative flex h-[478px] w-full flex-col items-center rounded-md border border-gray-200 bg-white pb-[27px] pt-[30px] shadow-base">
           {!hasNoData ? (
             <>
-              <div className="flex gap-[13px] text-lg font-bold">
-                <div>Q</div>
-                <div>{previousTitle}</div>
+              <div className="flex flex-col gap-4 pl-4">
+                <div className="flex w-full gap-1 pr-5 text-lg font-bold">
+                  <div className="flex h-[26px] min-w-[26px] items-center justify-center">
+                    Q
+                  </div>
+                  <p className="w-full">{previousTitle}</p>
+                </div>
+                <ul className="flex max-h-[291px] w-full flex-col gap-6 pr-[30px]">
+                  {answerListData?.pages[0].answers
+                    .slice(0, 3)
+                    .map((answerData, index) => (
+                      <li key={answerData.userId} className="flex flex-1 gap-1">
+                        <div className="flex">
+                          <div className="flex size-[26px] flex-1 items-center justify-center text-lg font-semibold text-gray-400">
+                            {index + 1}
+                          </div>
+                        </div>
+                        <div className="flex w-full flex-col gap-1">
+                          <div className="flex items-center gap-1">
+                            <div className="relative size-6 overflow-hidden rounded-full">
+                              <Image
+                                src={
+                                  answerData.profileImg.startsWith('https')
+                                    ? answerData.profileImg
+                                    : DEFAULT_IMAGE_URL
+                                }
+                                fill
+                                alt="icon"
+                              />
+                            </div>
+                            <div className="text-sm font-medium text-gray-600">
+                              {answerData.nickname}
+                            </div>
+                          </div>
+                          <div className="line-clamp-2 break-all text-base font-medium text-gray-700">
+                            {answerData.content}
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                </ul>
               </div>
-              <ul className="flex min-h-[291px] w-full flex-col gap-6">
-                {answerListData?.pages[0].answers
-                  .slice(0, 3)
-                  .map((answerData, index) => (
-                    <li key={answerData.userId} className="flex gap-[13px]">
-                      <div className="text-lg font-semibold text-gray-400">
-                        {index + 1}
-                      </div>
-                      <div className="flex flex-col gap-1">
-                        <div className="flex items-center gap-1">
-                          <div className="relative size-6 overflow-hidden rounded-full">
-                            <Image
-                              src={
-                                answerData.profileImg.startsWith('https')
-                                  ? answerData.profileImg
-                                  : DEFAULT_IMAGE_URL
-                              }
-                              fill
-                              alt="icon"
-                            />
-                          </div>
-                          <div className="text-sm font-medium text-gray-600">
-                            {answerData.nickname}
-                          </div>
-                        </div>
-                        <div className="line-clamp-2 break-all text-base font-medium text-gray-700">
-                          {answerData.content}
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-              </ul>
-
               <button
-                className="mt-2 w-full border-t border-gray-200 pt-3 text-center text-base text-gray-500"
+                className="absolute bottom-[27px] w-full border-t border-gray-200 pt-3 text-center text-base text-gray-500"
                 onClick={handleClickAnswerViewBtn}
               >
                 Best 답변 모두 보기
