26ab195b89379123a8b9dac0664bee8d323d7103
yeooonn <rladuswjd0303@naver.com>
2024-10-06 22:34:16 +0900

    Feat: 백문백답 모바일 버전 개발 완료

--- src/app/(routes)/solved/components/onehundred-qa/index.tsx
+++ src/app/(routes)/solved/components/onehundred-qa/index.tsx
@@ -18,9 +18,28 @@ interface QaDataType {
 
 const OnehundredQa = ({ accessToken }: { accessToken: string }) => {
   const [qaData, setQaData] = useState<QaDataType[]>([]);
+  const [isMobile, setIsMobile] = useState<boolean>(false); // 모바일 여부 상태
   const { data } = useUserChallengesProgress({
     accessToken,
   });
+
+  // 윈도우 사이즈 체크
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 375);
+  };
+
+  useEffect(() => {
+    // 컴포넌트가 처음 렌더링될 때 현재 창 크기를 기준으로 상태를 설정
+    handleResize();
+
+    // 윈도우 리사이즈 이벤트 리스너 등록
+    window.addEventListener('resize', handleResize);
+
+    // 컴포넌트가 언마운트될 때 리스너 제거
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     if (data) {
@@ -31,48 +50,48 @@ const OnehundredQa = ({ accessToken }: { accessToken: string }) => {
             title: '최다 빈출 기본질문',
             total: 10,
             done: data[0].count,
-            top: 25,
-            left: 0,
+            top: isMobile ? -50 : 25,
+            left: isMobile ? 20 : 0,
           },
           {
             image: 'lv2',
             title: '직무역량 & 경험 1',
             total: 18,
             done: data[1].count,
-            top: 25,
-            left: 80,
+            top: isMobile ? -50 : 25,
+            left: isMobile ? 50 : 80,
           },
           {
             image: 'lv3',
             title: '직무역량 & 경험 2',
             total: 18,
             done: data[2].count,
-            top: 221,
-            left: 60,
+            top: isMobile ? -15 : 221,
+            left: isMobile ? 20 : 60,
           },
           {
             image: 'lv4',
             title: '회사 로열티 & 컬쳐핏 1',
             total: 19,
             done: data[3].count,
-            top: 90,
-            left: 105,
+            top: isMobile ? -15 : 90,
+            left: isMobile ? 50 : 105,
           },
           {
             image: 'lv5',
             title: '회사 로열티 & 컬쳐핏 2',
             total: 20,
             done: data[4].count,
-            top: 290,
-            left: -125,
+            top: isMobile ? 20 : 290,
+            left: isMobile ? 20 : -125,
           },
           {
             image: 'lv6',
             title: '가치관 & 비전',
             total: 15,
             done: data[5].count,
-            top: 290,
-            left: -45,
+            top: isMobile ? 20 : 290,
+            left: isMobile ? 45 : -45,
           },
         ]);
       }
@@ -83,61 +102,61 @@ const OnehundredQa = ({ accessToken }: { accessToken: string }) => {
           title: '최다 빈출 기본질문',
           total: 10,
           done: 0,
-          top: 25,
-          left: 0,
+          top: isMobile ? -50 : 25,
+          left: isMobile ? 20 : 0,
         },
         {
           image: 'lv2',
           title: '직무역량 & 경험 1',
           total: 18,
           done: 0,
-          top: 25,
-          left: 80,
+          top: isMobile ? -50 : 25,
+          left: isMobile ? 50 : 80,
         },
         {
           image: 'lv3',
           title: '직무역량 & 경험 2',
           total: 18,
           done: 0,
-          top: 221,
-          left: 60,
+          top: isMobile ? -15 : 221,
+          left: isMobile ? 20 : 60,
         },
         {
           image: 'lv4',
           title: '회사 로열티 & 컬쳐핏 1',
           total: 19,
           done: 0,
-          top: 90,
-          left: 105,
+          top: isMobile ? -15 : 90,
+          left: isMobile ? 50 : 105,
         },
         {
           image: 'lv5',
           title: '회사 로열티 & 컬쳐핏 2',
           total: 20,
           done: 0,
-          top: 290,
-          left: -125,
+          top: isMobile ? 20 : 290,
+          left: isMobile ? 20 : -125,
         },
         {
           image: 'lv6',
           title: '가치관 & 비전',
           total: 15,
           done: 0,
-          top: 290,
-          left: -45,
+          top: isMobile ? 20 : 290,
+          left: isMobile ? 45 : -45,
         },
       ]);
     }
-  }, [data]);
+  }, [data, isMobile]);
 
   return (
-    <>
+    <div className="w-full">
       <Heading />
-      <div className="mb-[42px] rounded-md border border-gray-200 bg-white p-[28px] shadow-base">
+      <div className="mb-[42px] w-full rounded-md border border-gray-200 bg-white p-[28px] shadow-base mobile:px-[10px] mobile:py-[20px]">
         <Progress accessToken={accessToken} />
         <CircularDeterminate qaData={qaData} accessToken={accessToken} />
       </div>
-    </>
+    </div>
   );
 };
 export default OnehundredQa;
--- src/views/solved/index.tsx
+++ src/views/solved/index.tsx
@@ -42,7 +42,7 @@ export const Solved = async () => {
           alt="banner"
         />
       </div>
-      <div className="flex flex-1 flex-col gap-2 px-4 md:px-0 lg:px-0">
+      <div className="flex w-[588px] flex-1 flex-col gap-2 px-4 md:px-0 lg:px-0 mobile:w-[50%]">
         <HydrationBoundary state={dehydrate(queryClient)}>
           <TogetherSolvedSection />
         </HydrationBoundary>
