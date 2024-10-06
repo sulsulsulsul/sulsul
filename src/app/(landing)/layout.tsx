f9b81450d246942e797681560c7828fe5d7ad856
ggjiny <jnss599@gmail.com>
2024-10-07 01:03:57 +0900

    fix: 전체 레이아웃 수정

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -8,11 +8,7 @@ const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-[910px] overflow-hidden bg-gray-50">
       <Header />
-      <div className="min-h-[calc(100vh-60px)]">
-        <div className="container px-0 pt-[32px] sm:px-3 md:px-5">
-          {children}
-        </div>
-      </div>
+      <div className="min-h-[calc(100vh-60px)]">{children}</div>
     </div>
   );
 };
