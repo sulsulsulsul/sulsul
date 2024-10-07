1a79659a481c5f59ed9d1b945afecfd972f08839
ggjiny <jnss599@gmail.com>
2024-10-07 17:51:24 +0900

    style: 도네이션 버튼 위치 변경

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -2,6 +2,8 @@ import { FunctionComponent, PropsWithChildren } from 'react';
 
 import { Header } from '@/components/layouts/header';
 
+import DonationButton from '../(without-header)/donation/components/donation-button';
+
 interface LayoutProps extends PropsWithChildren {}
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
@@ -9,6 +11,7 @@ const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
     <div className="min-h-[910px] overflow-hidden bg-gray-50">
       <Header />
       <div className="min-h-[calc(100vh-60px)]">{children}</div>
+      <DonationButton />
     </div>
   );
 };
