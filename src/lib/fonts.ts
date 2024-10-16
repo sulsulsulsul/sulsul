75cc0f2e794b93b4d4c9c793872e08e44ffb934e
ggjiny <jnss599@gmail.com>
2024-10-16 22:33:02 +0900

    style: 폰트 수정

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,4 +3,6 @@ import localFont from 'next/font/local';
 export const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
   variable: '--font-pretendard',
+  display: 'swap',
+  weight: '45 920',
 });
